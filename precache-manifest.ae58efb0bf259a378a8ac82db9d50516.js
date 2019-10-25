9922b4a68d38f2f9326e7816ee560a7252f056f7
Idris <idrisaboladeo@gmail.com>
2019-10-25 16:48:17 +0100

    Updates

---
+++ precache-manifest.ae58efb0bf259a378a8ac82db9d50516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafba12805474ac562e7383f6614dc3d",
+    "url": "/rolodex-app/index.html"
+  },
+  {
+    "revision": "914b3b1ed3e419716d80",
+    "url": "/rolodex-app/static/css/main.ba3febce.chunk.css"
+  },
+  {
+    "revision": "9707a99f1e73b0bbce40",
+    "url": "/rolodex-app/static/js/2.7f67743b.chunk.js"
+  },
+  {
+    "revision": "914b3b1ed3e419716d80",
+    "url": "/rolodex-app/static/js/main.225089de.chunk.js"
+  },
+  {
+    "revision": "71f483039b29d38403d9",
+    "url": "/rolodex-app/static/js/runtime-main.ef534984.js"
+  }
+]);
